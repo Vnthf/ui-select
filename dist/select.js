7a1631a427003a24782444fd0cda50d5f4ea3234
김정기/두레이개발실 <kim.jeongki@nhnent.com>
2017-06-06 23:29:27 +0900

    add uisOpenClose directive

--- dist/select.js
+++ dist/select.js
@@ -295,6 +295,35 @@
     };
   }]);
 
+  uis.directive('uisOpenClose', ['$parse', '$timeout', function ($parse, $timeout) {
+    return {
+      restrict: 'A',
+      require: 'uiSelect',
+      link: function (scope, element, attrs, $select) {
+        var openFlagParse = $parse(attrs.uisOpen);
+        $select.onOpenCloseCallback = $parse(attrs.uisOpenClose);
+        scope.$watch('$select.open', function (isOpen, previousState) {
+          if (isOpen !== previousState) {
+            $timeout(function () {
+              openFlagParse(scope, {isOpen: isOpen});
+              $select.onOpenCloseCallback(scope, {
+                isOpen: isOpen
+              });
+            });
+          }
+        });
+
+        scope.$watch(openFlagParse, function(newVal){
+          if(newVal === true){
+            $select.activate();
+          }else if(newVal === false){
+            $select.close();
+          }
+        });
+      }
+    };
+  }]);
+
   /**
    * Contains ui-select "intelligence".
    *

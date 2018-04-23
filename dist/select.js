96af527e792111ff929be831db6c5d26bfb720e8
김부승/Dooray 개발실/NE <buseung.kim@nhnent.com>
2018-04-23 17:54:34 +0900

    탭 키에 대한 동작 수정 검색어가 있는 경우: 기본 탭동작 막고 선택 검색어가 없는 경우: 기본 탭동작하고 선택되지 않음

--- dist/select.js
+++ dist/select.js
@@ -6,7 +6,7 @@
  */
 
 
-(function () { 
+(function () {
 "use strict";
 
 var isMacOS = /\bMac OS\b/.test(window.navigator.userAgent);
@@ -1065,6 +1065,10 @@ uis.controller('uiSelectCtrl',
     if (~[KEY.ENTER,KEY.ESC].indexOf(key)){
       e.preventDefault();
       e.stopPropagation();
+    }
+
+    if (KEY.TAB === key) {
+      ctrl.search ? e.preventDefault() : ctrl.close();
     }
 
     // if(~[KEY.ESC,KEY.TAB].indexOf(key)){
@@ -2835,12 +2839,12 @@ uis.service('uisRepeatParser', ['uiSelectMinErr','$parse', function(uiSelectMinE
       throw uiSelectMinErr('iexp', "Expected expression in form of '_item_ in _collection_[ track by _id_]' but got '{0}'.",
               expression);
     }
-    
-    var source = match[5], 
+
+    var source = match[5],
         filters = '';
 
     // When using (key,value) ui-select requires filters to be extracted, since the object
-    // is converted to an array for $select.items 
+    // is converted to an array for $select.items
     // (in which case the filters need to be reapplied)
     if (match[3]) {
       // Remove any enclosing parenthesis
@@ -2850,7 +2854,7 @@ uis.service('uisRepeatParser', ['uiSelectMinErr','$parse', function(uiSelectMinE
       if(filterMatch && filterMatch[1].trim()) {
         filters = filterMatch[1];
         source = source.replace(filters, '');
-      }      
+      }
     }
 
     return {
@@ -2866,7 +2870,7 @@ uis.service('uisRepeatParser', ['uiSelectMinErr','$parse', function(uiSelectMinE
           expression += ' track by ' + this.trackByExp;
         }
         return expression;
-      } 
+      }
     };
 
   };
--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -667,6 +667,10 @@ uis.controller('uiSelectCtrl',
       e.stopPropagation();
     }
 
+    if (KEY.TAB === key) {
+      ctrl.search ? e.preventDefault() : ctrl.close();
+    }
+
     // if(~[KEY.ESC,KEY.TAB].indexOf(key)){
     //   //TODO: SEGURO?
     //   ctrl.close();
